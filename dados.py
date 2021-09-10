89e91273956e237ab2cb14386b46ba34dadd5033
kevin171fsa <90431457+kevin171fsa@users.noreply.github.com>
2021-09-10 09:18:28 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 que = {}
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-POST_CANAL = getenv("POST_CANAL", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "1978585012:AAH5twyVYxgpKkF1QDMSBbZoJBayLpf9TSs")
+POST_CANAL = getenv("POST_CANAL", "1490373082")
