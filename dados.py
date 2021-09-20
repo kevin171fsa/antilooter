2766fa53fd753859814b39ebadb9a1ad4363a777
kevin171fsa <90431457+kevin171fsa@users.noreply.github.com>
2021-09-19 22:04:17 -0300

    Delete dados.py

--- dados.py
+++
@@ -1,8 +0,0 @@
-from os import getenv
-
-from dotenv import load_dotenv
-
-load_dotenv()
-que = {}
-BOT_TOKEN = getenv("BOT_TOKEN", "1978585012:AAH5twyVYxgpKkF1QDMSBbZoJBayLpf9TSs")
-POST_CANAL = getenv("POST_CANAL", "")
